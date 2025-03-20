82953e7ce16d2a3407f8a759df47f996355afd36
Hema Bindra <hbindra11@yahoo.com>
2025-03-20 13:24:07 +0100

    just testing

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -6,8 +6,8 @@ const AppContext = createContext();
 
 const useAppContext = () => {
   const context = useContext(AppContext);
-  if (!context)
-    throw new Error("useAppContext must be used within AppContextProvider");
+  // if (!context)
+  //   throw new Error("useAppContext must be used within AppContextProvider");
   return context;
 };
 
