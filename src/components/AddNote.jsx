33b343cdcc002fb4b271bf4e27445eed0bc7dbad
Hema Bindra <hbindra11@yahoo.com>
2025-03-20 13:43:33 +0100

    yet another bug fix

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,4 +1,4 @@
-import { useAppContext } from "../context/AppContext.js";
+import { useAppContext } from "../context/appContext.js";
 import { storeNotes } from "../modules/storage";
 import { useNavigate } from "react-router-dom";
 import SelectCategories from "./SelectCategories";
