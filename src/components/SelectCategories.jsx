5f8b25b5b875eb891198baf156828d8320bb94a5
Hema Bindra <hbindra11@yahoo.com>
2025-07-08 17:02:27 +0200

    can assign multiple categories to a note now

--- src/components/SelectCategories.jsx
+++ src/components/SelectCategories.jsx
@@ -1,26 +1,42 @@
 import { fetchCategories } from "../modules/storage";
-const SelectCategories = () => {
+
+const SelectCategories = ({ selected = [], setSelected }) => {
   const AllCategories = fetchCategories();
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setSelected((prev) =>
+      prev.includes(value)
+        ? prev.filter((cat) => cat !== value)
+        : [...prev, value]
+    );
+  };
+
   return (
     <>
       <fieldset className="text-slate-50">
         <legend className="font-bold text-lg">Choose your categories:</legend>
-<br></br>
+        <br />
         <div>
           {AllCategories.map((aCategory) => (
             <li
-              key={crypto.randomUUID()}
+              key={aCategory.category}
               className="flex items-center mb-2 justify-between"
             >
               <input
                 type="checkbox"
-                //checked={}
-                //onChange={}
+                value={aCategory.category}
+                checked={selected.includes(aCategory.category)}
+                onChange={handleChange}
                 className="mr-2"
               />
               {aCategory.category}
             </li>
           ))}
+        </div>
+        {/* Example: show selected categories */}
+        <div className="mt-4 text-slate-200">
+          Selected: {selected.join(", ")}
         </div>
       </fieldset>
     </>
